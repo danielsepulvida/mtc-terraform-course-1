27ea3cdb3f421a71eb2071d9928945cccbf45f47
Daniel Sepulvida <dansepulvida@gmail.com>
2025-10-11 00:29:04 -0300

    Nested Blocks versão 2 - error by limitation in the Github

--- terraform-code/outputs.tf
+++ terraform-code/outputs.tf
@@ -1,5 +1,10 @@
 output "clone-urls" {
-  value       = { for i in github_repository.mtc-repo-1 : i.name => [i.ssh_clone_url, i.http_clone_url] }
+  value = { for i in github_repository.mtc-repo-1 : i.name => {
+    ssh-clone_url  = i.ssh_clone_url,
+    http-clone_url = i.http_clone_url,
+    pages-url      = i.pages[0].html_url
+    }
+  }
   description = "Repository Names and URL"
   sensitive   = false
 }
