386f98e5b338617f7190720a69eb163e4e0ef2cb
danielsepulvida <danielsepulvida@homelab-network.io>
2025-11-03 10:44:37 -0300

    refactored repos module for automation

--- terraform-code/modules/repos/main.tf
+++ terraform-code/modules/repos/main.tf
@@ -19,16 +19,14 @@ module "deploy-key" {
   repo_name = each.key
 }
 
-# module "info-page" {
-#   source           = "./modules/info-page"
-#   repos            = { for k, v in module.repos["prod"].clone-urls : k => v }
-#   run_provisioners = false
-# }
-
 output "repo-info" {
   value = { for k, v in module.repos : k => v.clone-urls }
 }
 
 output "repo-list" {
   value = flatten([for k, v in module.repos : keys(v.clone-urls) if k == "dev"])
+}
+
+output "clone_urls" {
+  value = module.repos
 }
